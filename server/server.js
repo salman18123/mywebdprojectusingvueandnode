c4ea359d1f6f3ac75ebfec97009403d025299abf
salman <salmanvit12345@gmail.com>
2018-01-08 16:06:53 +0530

    minor changes

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 const express=require('express')
 const cors=require('cors')
 
+
 const app=express()
+const port=process.env.PORT|| 9090
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cors())
 app.use('/',require('./routes'))
-app.listen(9090,()=>{
+
+app.listen(port,()=>{
     console.log("started server")
 })
